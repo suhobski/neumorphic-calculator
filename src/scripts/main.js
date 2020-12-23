e912962c13d0e03d688b98d9e6ae531f86c3a74c
suhobski <suhobski@mail.ru>
2020-12-23 15:30:56 +0300

    feat: add keyboard variables

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+// Переменные кнопок операций
+const clear = document.querySelector('.keyboard__btn--clear')
+const plusMinus = document.querySelector('.keyboard__btn--plus-minus')
+const percent = document.querySelector('.keyboard__btn--percent')
+const dot = document.querySelector('.keyboard__btn--dot')
+const equal = document.querySelector('.keyboard__btn--equal')
+
+
+const divide = document.querySelector('.keyboard__btn--divide')
+const multiply = document.querySelector('.keyboard__btn--multiply')
+const minus = document.querySelector('.keyboard__btn--minus')
+const plus = document.querySelector('.keyboard__btn--plus')
+
+// Переменные числовых кнопок
+const zero = document.querySelector('.keyboard__btn--zero')
+const one = document.querySelector('.keyboard__btn--one')
+const two = document.querySelector('.keyboard__btn--two')
+const three = document.querySelector('.keyboard__btn--three')
+const four = document.querySelector('.keyboard__btn--four')
+const five = document.querySelector('.keyboard__btn--five')
+const six = document.querySelector('.keyboard__btn--six')
+const seven = document.querySelector('.keyboard__btn--seven')
+const eight = document.querySelector('.keyboard__btn--eight')
+const nine = document.querySelector('.keyboard__btn--nine')
+

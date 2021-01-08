14426382f460f74987845f18277e1d085b18182e
suhobski <suhobski@mail.ru>
2021-01-08 08:25:06 +0300

    fix: compute function

--- src/scripts/calculatorDetails/compute.js
+++ src/scripts/calculatorDetails/compute.js
@@ -1,23 +1,21 @@
-// Функция вычисления результата
+// Function for calculating the result from an array ========================================================
 
 export function compute(array) {
 
   while(true) {
-    // если длина массива равна 1, мы получили результат
+
+    // if the length of the array is 1, we got the result
     if (array.length === 1) return array[0]
 
-    // на всякий случай, чтобы не повис браузер
-    if (array.length === 2) return array[0] + array[1]
-  
-    if (array.includes("×")) {
-      let index = array.indexOf("×") 
+    if (array.includes("×") || array.includes("*")) {
+      let index = (array.includes("×")) ? array.indexOf("×") : array.indexOf("*")
       let result = parseFloat(array[index-1]) * parseFloat(array[index+1])
       array.splice(index-1, 3, String(Number(result.toFixed(12))))
       continue  
     }    
     
-    if (array.includes("÷")) {
-      let index = array.indexOf("÷")
+    if (array.includes("÷") || array.includes("/")) {
+      let index = (array.includes("÷")) ? array.indexOf("÷") : array.indexOf("/") 
       let result = parseFloat(array[index-1]) / parseFloat(array[index+1])
       array.splice(index-1, 3, String(Number(result.toFixed(12))))
       continue  
